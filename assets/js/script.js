26100b82e8e974295a07cdfa2b025c5516f96b74
George Saad <gsparqcode@gmail.com>
2022-04-04 16:10:11 -0400

    Main page + API call for meal

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,9 @@ var cockTailUrl = "https://the-cocktail-db.p.rapidapi.com/random.php";
 var yumlyUrl = "https://yummly2.p.rapidapi.com/categories/list";
 var MealsUrl = "https://themealdb.p.rapidapi.com/latest.php";
 
-// get element by ID for each link in NAVBAR
+// get element by ID for PARENT
 var drinksList = document.querySelector(".drinks-list");
+var randomMeal = document.getElementById("RandomMealImg");
 
 // // API CALL 1 COCKTAILDB
 // const cockTails = {
@@ -41,18 +42,22 @@ var drinksList = document.querySelector(".drinks-list");
 //   .catch((err) => console.error(err));
 
 // // API CALL 3 TheMealDB
-// const Meals = {
-//   method: "GET",
-//   headers: {
-//     "X-RapidAPI-Host": "themealdb.p.rapidapi.com",
-//     "X-RapidAPI-Key": "37f2a9a7cemsh5b611bc49e7a303p1e79e6jsn6b86cc035e5d",
-//   },
-// };
+const Meals = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "themealdb.p.rapidapi.com",
+    "X-RapidAPI-Key": "37f2a9a7cemsh5b611bc49e7a303p1e79e6jsn6b86cc035e5d",
+  },
+};
 
-// fetch(MealsUrl, Meals)
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
+fetch(MealsUrl, Meals)
+  .then((response) => response.json())
+  .then(
+    (response) =>
+      console.log(response.meals[0].strMealThumb) +
+      randomMeal.setAttribute("src", response.meals[4].strMealThumb)
+  )
+  .catch((err) => console.error(err));
 //functions
 drinksList.addEventListener("click", function (event) {
   if (event.target && event.target.innerHTML === "Vodka") {
